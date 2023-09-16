c593bfbbd18bdea661f7f29abf63643a7955b466
yazan-metax <95860716+yazan-metax@users.noreply.github.com>
2023-09-16 16:14:09 +0300

    Update ft_putnbr_pf.c

--- ft_putnbr_pf.c
+++ ft_putnbr_pf.c
@@ -12,31 +12,6 @@
 
 #include "ft_printf.h"
 
-/* void	ft_putnbr_pf(int num, size_t *counter)
-{
-	int	i;
-
-	i = -1;
-	if (num == -2147483648)
-	{
-		ft_putstr_pf("-2147483648", counter);
-		return ;
-	}
-	else if (num < 0)
-	{
-		ft_putchar_pf('-', counter);
-		num *= i;
-	}
-	else if (num > 9)
-	{
-		ft_putnbr_pf(num / 10, counter);
-		ft_putchar_pf(num % 10 + 48, counter);
-	}
-	else
-	{
-		ft_putchar_pf(num + 48, counter);
-	}
-} */
 void	ft_putnbr_pf(int num, size_t *counter)
 {
 	if (num == -2147483648)
